d393e59c6edf6bb7cc72d86f4405bd849202400a
bs9934 <bs9934@g.yju.ac.kr>
2023-01-03 14:13:05 +0900

    230103

---
+++ src/main/java/com/experdb/configuration/mapper/ThresholdMapper.java
@@ -0,0 +1,17 @@
+package com.experdb.configuration.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@MapperScan
+public interface ThresholdMapper {
+    public List<Map<String, Object>> selectThresholds() throws Exception;
+    public int insertThresholds(HashMap<String,Object> map) throws Exception;
+    public int updateThresholds(HashMap<String,Object> map) throws Exception;
+    public int deleteThresholds(HashMap<String,Object> map) throws Exception;
+}
